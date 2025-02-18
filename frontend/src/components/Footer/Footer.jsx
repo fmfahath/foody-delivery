2221307e6fb93bc99e69927627a8f0b090a10cbb
fmfahath <fmfahath.dev@gmail.com>
2025-02-18 17:30:08 +0530

    frontend >> Footer -- Footer component created, css added

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" />
+                    <p><b>Tomoto</b> is dedicated to bringing you the best meals from your favorite local restaurants. Follow us on social media for the latest updates and offers. </p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="" />
+                        <img src={assets.linkedin_icon} alt="" />
+                        <img src={assets.twitter_icon} alt="" />
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>+94-7556645</li>
+                        <li>Contact@tomoto.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <p className='footer-copyright'>&copy; 2025 Tomoto. All rights </p>
+        </div>
+    )
+}
+
+export default Footer
