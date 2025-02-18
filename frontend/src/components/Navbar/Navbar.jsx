8af5d63af1d50e94a5eeb2f82f7878402a247162
fmfahath <fmfahath.dev@gmail.com>
2025-02-18 10:45:46 +0530

    frontend >> Navbar -- active menu state, css added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import { assets } from '../../assets/assets.js'
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState('home')
+
     return (
         <div className='navbar'>
             <img src={assets.logo} alt="logo" className='logo' />
             <ul className="navbar-menu">
-                <li>Home</li>
-                <li>Menu</li>
-                <li>Mobile App</li>
-                <li>Contact</li>
+                <li onClick={() => setMenu("home")} className={menu === 'home' ? 'active' : ''}>Home</li>
+                <li onClick={() => setMenu("menu")} className={menu === 'menu' ? 'active' : ''}>Menu</li>
+                <li onClick={() => setMenu("mobile")} className={menu === 'mobile' ? 'active' : ''}>Mobile App</li>
+                <li onClick={() => setMenu("contact")} className={menu === 'contact' ? 'active' : ''}>Contact</li>
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="search-icon" />
