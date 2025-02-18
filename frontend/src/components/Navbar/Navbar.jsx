25f43444e4d2186b63374788d661f2a62de363f9
fmfahath <fmfahath.dev@gmail.com>
2025-02-18 10:38:56 +0530

    frontend >> Navbar -- component created, css added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './navbar.css'
+import { assets } from '../../assets/assets.js'
 
 const Navbar = () => {
     return (
-        <div>navbar</div>
+        <div className='navbar'>
+            <img src={assets.logo} alt="logo" className='logo' />
+            <ul className="navbar-menu">
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Mobile App</li>
+                <li>Contact</li>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="search-icon" />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt='basket-icon' />
+                    <div className="dot"></div>
+                </div>
+                <button>Sign in</button>
+            </div>
+        </div>
     )
 }
 
