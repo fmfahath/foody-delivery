c90e56fa7613a6278b5ca83a37c57bde4d5055d0
fmfahath <fmfahath.dev@gmail.com>
2025-02-18 13:32:09 +0530

    frontend >> Headeer -- component created, css added

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
     return (
-        <div>header</div>
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order Your Favorite Food Here</h2>
+                <p>Crave. Click. Enjoy. From our kitchen to your doorstep, Delicious meals delivered fast and fresh. Satisfy your cravings with just a tap!</p>
+                <button>Order Now</button>
+            </div>
+        </div>
     )
 }
 
