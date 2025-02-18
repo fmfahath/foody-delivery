c40077ac3025dc0ecf76fe4f256576605e3c94ee
fmfahath <fmfahath.dev@gmail.com>
2025-02-18 16:46:04 +0530

    frontend >> FoodDisplay -- itemCount state removed from FoodItem.jsx, cartItems state, addTocart, removeCart fun added in StoreContextProvider, used in add-remove item button function in FoodItem.jsx

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({ id, name, price, description, image }) => {
 
-    const [itemCount, setItemCount] = useState(0)
+    const { cartItems, addToCart, removecart } = useContext(StoreContext)
 
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
                 <img className='food-item-image' src={image} alt="" />
                 {
-                    itemCount > 0 ?
+                    cartItems[id] ?
                         <div className="food-item-counter">
-                            <img src={assets.remove_icon_red} onClick={() => setItemCount(pre => pre - 1)} />
-                            <p>{itemCount}</p>
-                            <img src={assets.add_icon_green} onClick={() => setItemCount(pre => pre + 1)} />
+                            <img src={assets.remove_icon_red} onClick={() => removecart(id)} />
+                            <p>{cartItems[id]}</p>
+                            <img src={assets.add_icon_green} onClick={() => addToCart(id)} />
                         </div> :
-                        <img className='add' src={assets.add_icon_white} onClick={() => setItemCount(pre => pre + 1)} />
+                        <img className='add' src={assets.add_icon_white} onClick={() => addToCart(id)} />
                 }
             </div>
             <div className="food-item-info">
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,13 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = ({ children }) => {
 
+    const [cartItems, setCartItem] = useState({})
+
+    //add cart items
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+
+            setCartItem({ ...cartItems, [itemId]: 1 })
+        }
+        else {
+            setCartItem({ ...cartItems, [itemId]: cartItems[itemId] + 1 })
+        }
+    }
+
+    //remove items from cart
+    const removecart = (itemId) => {
+        setCartItem({ ...cartItems, [itemId]: cartItems[itemId] - 1 })
+    }
 
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItem,
+        addToCart,
+        removecart
     }
 
     return (
