4c1907b1c334de21de6db60b6ff9c282594198e4
fmfahath <fmfahath.dev@gmail.com>
2025-02-19 07:40:56 +0530

    frontend >> Cart -- Cart page created, css added

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const Cart = () => {
+
+    const { food_list, cartItems, setCartItem, removecart } = useContext(StoreContext)
+
     return (
-        <div>Cart</div>
+        <div className='cart'>
+            <div className="cart-items">
+                <div className="cart-items-title">
+                    <p>Items</p>
+                    <p>Title</p>
+                    <p>Price</p>
+                    <p>Quantity</p>
+                    <p>Total</p>
+                    <p>Remove</p>
+                </div>
+                <br />
+                <hr />
+                {food_list.map((item, index) => {
+                    if (cartItems[item._id] > 0) {
+
+                        return (
+                            <>
+                                <div className="cart-items-title cart-items-item">
+                                    <img src={item.image} alt={item.title} />
+                                    <p>{item.name}</p>
+                                    <p>${item.price}</p>
+                                    <p>{cartItems[item._id]}</p>
+                                    <p>${item.price * cartItems[item._id]}</p>
+                                    <p className='cross' onClick={() => removecart(item._id)}>X</p>
+                                </div>
+                                <hr />
+                            </>
+                        )
+                    }
+                })}
+            </div>
+            <div className="cart-bottom">
+                <div className="cart-total">
+                    <h2>Cart Total</h2>
+                    <div>
+                        <div className="cart-toal-details">
+                            <p>Subtotal</p>
+                            <p>{0}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-toal-details">
+                            <p>Delivery Fee</p>
+                            <p>{2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-toal-details">
+                            <p>Total</p>
+                            <p>{0}</p>
+                        </div>
+                    </div>
+                    <button>ROCEED TO CHECKOUT</button>
+                </div>
+                <div className="cart-promocode">
+                    <div>
+                        <p>If you have a promo code, enter it here</p>
+                        <div className='cart-promocode-input'>
+                            <input type="text" placeholder='promo Code' />
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
