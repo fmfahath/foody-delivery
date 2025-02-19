8fa4e69a255a79f96a4091e33816923706a54841
fmfahath <fmfahath.dev@gmail.com>
2025-02-19 08:09:14 +0530

    frontend >> Cart -- Navbar cart bucket dot item conditon added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './navbar.css'
 import { assets } from '../../assets/assets.js'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext.jsx'
 
 const Navbar = ({ setShowLogin }) => {
 
     const [menu, setMenu] = useState('home')
+    const { getCartTotal } = useContext(StoreContext)
 
     return (
         <div className='navbar'>
@@ -20,7 +22,7 @@ const Navbar = ({ setShowLogin }) => {
                 <img src={assets.search_icon} alt="search-icon" />
                 <div className="navbar-search-icon">
                     <Link to={'/cart'}><img src={assets.basket_icon} alt='basket-icon' /></Link>
-                    <div className="dot"></div>
+                    <div className={getCartTotal() > 0 ? "dot" : ""}></div>
                 </div>
                 <button onClick={() => setShowLogin(true)}>Sign in</button>
             </div>
