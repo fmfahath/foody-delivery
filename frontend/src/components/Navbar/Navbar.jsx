2a771c8ec8ad213bb053484b3281096b80790fa0
fmfahath <fmfahath.dev@gmail.com>
2025-02-20 20:57:36 +0530

    frontend >> Navbar -- profile drop dwon menu added in Navbar.jsx, css added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { StoreContext } from '../../context/StoreContext.jsx'
 const Navbar = ({ setShowLogin }) => {
 
     const [menu, setMenu] = useState('home')
-    const { getCartTotal } = useContext(StoreContext)
+    const { getCartTotal, token, setToken } = useContext(StoreContext)
 
     return (
         <div className='navbar'>
@@ -24,7 +24,18 @@ const Navbar = ({ setShowLogin }) => {
                     <Link to={'/cart'}><img src={assets.basket_icon} alt='basket-icon' /></Link>
                     <div className={getCartTotal() > 0 ? "dot" : ""}></div>
                 </div>
-                <button onClick={() => setShowLogin(true)}>Sign in</button>
+                {token ?
+                    <button onClick={() => setShowLogin(true)}>Sign in</button>
+                    :
+                    <div className='navbar-profile'>
+                        <img src={assets.profile_icon} alt="" />
+                        <ul className="nav-profile-dropdown">
+                            <li><img src={assets.bag_icon} /><p>Orders</p></li>
+                            <hr />
+                            <li><img src={assets.logout_icon} /><p>Logout</p></li>
+                        </ul>
+                    </div>
+                }
             </div>
         </div >
     )
