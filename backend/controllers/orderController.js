656d25f5bf91ae686c8eacbf2f44a27834a97ca9
fmfahath <fmfahath.dev@gmail.com>
2025-02-21 10:33:08 +0530

    backend >> order -- orderModel, orderCOntroller, orderRoute created

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,8 @@
+import orderModel from "../models/orderMOdel.js";
+import userModel from "../models/userModel.js";
+import stripe from 'stripe'
+
+//placing order for frontend
+export const placeOrder = async (req, res) => {
+
+}
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
 import userRouter from './routes/userRoute.js'
 import cardRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
 
 
 
@@ -24,6 +25,7 @@ app.use('/api/images', express.static('uploads'))
 app.use('/api/food', foodRouter)
 app.use('/api/user', userRouter)
 app.use('/api/cart', cardRouter)
+app.use('/api/order', orderRouter)
 
 app.get('/', (req, res) => {
     res.send('Node Runnning..')
