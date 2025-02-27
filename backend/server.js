caa33db0b242cdc1e12b95ca79fc5c732d7abea0
fmfahath <fmfahath.dev@gmail.com>
2025-02-27 08:00:21 +0530

    Vercel deployment >> cors ERROR - changing cors config modified

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 4000
 
 //middleware 
 app.use(express.json())
-app.use(cors({ origin: 'https://foody-delivery-frontend.vercel.app' }));
+app.use(cors());
 
 //connect Database
 connectDB()
